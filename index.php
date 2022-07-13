2882307bd8ab21f0be4e349ae26b16cec621eb91
anton <sims0204@gmail.com>
2022-07-13 19:16:48 +0400

    init

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="./img/svg/list-style.svg" type="image/svg">
+  <link rel="shortcut icon" href="./img/svg/list-style.svg" type="image/svg">
+  <title>NeoCinema</title>
+  <link rel="stylesheet" href="./css/style.css" />
+</head>
+
+<body>
+  <?php
+        include "include/header.php";
+    ?>
+  <main>
+
+
+  </main>
+
+
+  <?php
+        include "include/footer.php";
+    ?>
+</body>
+
+</html>
